ff28d398a0e8c86b06b57280b96f5374bd94cabd
sagar <7208sk@gmail.com>
2021-10-09 23:46:20 +0530

    Fibonacc series

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Logical_programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            
+                int Number1 = 0, Number2 = 1, Number3, i, Number;
+                Console.WriteLine("Enter the Number of Elements: ");
+                Number = int.Parse(Console.ReadLine());
+                Console.Write(Number1 + " " + Number2 + " ");
+                for (i = 2; i < Number; i++)
+                {
+                    Number3 = Number1 + Number2;
+                    Console.Write(Number3 + " ");
+                    Number1 = Number2;
+                    Number2 = Number3;
+                }
+            }
+    }
+}
